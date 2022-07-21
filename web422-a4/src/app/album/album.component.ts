4b6abe9c29914cfb559afbc63baad02c92837cf3
Ruban Manoj Paul <ruban-manoj-paul@myseneca.ca>
2022-07-20 22:31:09 -0400

    commit

--- web422-a4/src/app/album/album.component.ts
+++ web422-a4/src/app/album/album.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
-import albumData from '../data/album.json';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MusicDataService } from '../music-data.service';
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
-  styleUrls: ['./album.component.css']
+  styleUrls: ['./album.component.css'],
 })
 export class AlbumComponent implements OnInit {
-
+  private albumSubscribe!: Subscription;
   album: any;
-  constructor() { }
+  private id: any;
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
+    private musicDataService: MusicDataService
+  ) {}
 
   ngOnInit(): void {
-    this.album = albumData
+    this.id = this.route.snapshot.params['id'];
+    this.albumSubscribe = this.musicDataService
+      .getAlbumById(this.id)
+      .subscribe((data) => {
+        return (this.album = data);
+      });
   }
 
+  addToFavourites(trackID: any) {
+    if (this.musicDataService.addToFavourites(trackID)) {
+      this.snackBar.open('Adding to Favourites...', 'Done', { duration: 1500 });
+    }
+  }
+
+  ngOnDestroy() {
+    this.albumSubscribe?.unsubscribe();
+  }
 }
