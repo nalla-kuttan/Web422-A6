c4900dedd71c80ad9bcb1d73ee5b8d5611f60362
Ruban Manoj Paul <ruban-manoj-paul@myseneca.ca>
2022-08-05 14:49:04 -0400

    a6 commit

--- web422-a6/src/app/app.component.ts
+++ web422-a6/src/app/app.component.ts
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB422 – Assignment 05
+*  WEB422 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
 *  assignment has been copied manually or electronically from any other source (including web sites) or 
 *  distributed to other students.
 * 
-*  Name: Ruban Manoj Paul  |  Student ID: 144008208  |  Date: 20th July 2022
+*  Name: Ruban Manoj Paul  |  Student ID: 144008208  |  Date: 5th August 2022
 *
 ********************************************************************************/ 
 import { Component } from '@angular/core';
